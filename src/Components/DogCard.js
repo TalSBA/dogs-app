fb96e9c8c51fb9f1f02b28dba210cff22f3009e9
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-04-27 20:15:27 +0300

    filter dogs by text

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import "../Styles/DogCard.css";
 
 function DogCard({breed, image}) {
   return (
-    <div>
-      <Card >
+    <div className="c-dog-card">
+      <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={image} />
         <Card.Body>
           <Card.Title>{breed}</Card.Title>
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,11 +1,51 @@
-import React, { useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import DogCard from "../Components/DogCard";
 import SearchBox from "../Components/SearchBox";
+import DogModel from "../Model/DogModel";
 import "../Styles/Gallery.css";
 
 function Gallery(props) {
+  const [dogs, setDogs] = useState([]);
   const [searchText, setSearchText] = useState("");
-  function handleSearchChange() {}
+
+  useEffect(() => {
+    loadDogs();
+  }, []);
+
+  function loadDogs() {
+    setDogs([]);
+    let count = 10;
+    axios.get("https://dog.ceo/api/breeds/list/all").then((response) => {
+      for (const breed in response.data.message) {
+        if (count === 0) {
+          break;
+        }
+        count--;
+        axios
+          .get("https://dog.ceo/api/breed/" + breed + "/images/random")
+          .then((response2) => {
+            setDogs((oldArr) => [
+              ...oldArr,
+              new DogModel(breed, response2.data.message),
+            ]);
+          });
+      }
+    });
+  }
+
+  function handleSearchChange(newSearchText) {
+    setSearchText(newSearchText);
+  }
+
+  let dogsCard;
+  if (dogs) {
+    dogsCard = dogs.filter((dog) => {
+      return dog.breedName.toLowerCase().includes(searchText.toLowerCase());
+    });
+  }
+
   return (
     <Container className="p-gallery">
       <div className="head-page">
@@ -14,8 +54,16 @@ function Gallery(props) {
           searchText={searchText}
           onSearchChange={handleSearchChange}
         />
-        <Button>Update Images</Button>
+        <Button onClick={() => loadDogs()}>Update Images</Button>
       </div>
+      <Row>
+        {dogsCard &&
+          dogsCard.map((dog, index) => (
+            <Col key={index} lg={6} md={7}>
+              <DogCard key={index} breed={dog.breedName} image={dog.image} />
+            </Col>
+          ))}
+      </Row>
     </Container>
   );
 }

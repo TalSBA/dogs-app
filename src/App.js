3e35734be7fd1d3e9b59d7373ed76c78e784cdcd
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-04-27 15:04:02 +0300

    Create Home Page content + add react routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './Styles/App.css';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import "./Styles/App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../Styles/Home.css";
 
 function Home(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="p-home">
+      <h1>Dogs Book</h1>
+      <h4>Man's Best Friend</h4>
+      <Link to="/Gallery">
+        <Button>Woof!</Button>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;

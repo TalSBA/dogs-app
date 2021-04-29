d606e25db8290bbd0757efc05f161a4d70bf829b
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-04-29 10:44:54 +0300

    add icon on card hover

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../Styles/DogCard.css";
+import dogIcon from "../Styles/dog.png";
 
-function DogCard({breed, image}) {
+function DogCard({ breed, image }) {
   return (
     <div className="c-dog-card">
       <Card>
         <Card.Img variant="top" src={image} />
         <Card.Body>
-          <Card.Title>{breed}</Card.Title>
+          <Card.Title>{breed} </Card.Title>{" "}
+          <span className="dog-icon">
+            <img src={dogIcon}></img>
+          </span>
         </Card.Body>
       </Card>
     </div>

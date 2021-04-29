a3d2a831fa70fe23e243b44468df24188a242163
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-04-29 20:14:11 +0300

    fix layout css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,17 +2,21 @@ import React from "react";
 import "../Styles/Home.css";
 import Header from "../Components/Header";
 import dogImg from "../Styles/dog.png";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function Home(props) {
   return (
     <div className="p-home">
       <Header />
-        <div className="home-img">
-          <h1>
-            Think <br /> P<img src={dogImg}></img>wsitive!
-          </h1>
-        </div>
+      <div className="home-img">
+        <Row>
+          <Col>
+            <h1>
+              Think <br /> P<img src={dogImg}></img>wsitive!
+            </h1>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
